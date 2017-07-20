51787b35bac2727a8acda47a70458a04078328f3
chenfei <yuchenggit@qq.com>
2017-07-20 19:12:07 +0800

    临时

--- HelloOpenGL/opengl8.cpp
+++ HelloOpenGL/opengl8.cpp
@@ -18,17 +18,69 @@ void init(void)
 	glClearColor(1.0, 1.0, 1.0, 0.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
+	gluOrtho2D(-100.0, 100.0, -100.0, 100.0);
+	glMatrixMode(GL_MODELVIEW);
+}
+void triangle(wcPt2D * verts)
+{
+	GLint k;
+	glBegin(GL_TRIANGLES);
+	for (k = 0; k < 3; k++)
+		glVertex2f(verts[k].x, verts[k].y);
+	glEnd();
+}
+void displayFunc(void)
+{
+	wcPt2D verts[3] = { {-50.0, -25.0},{50.0, -25.0},{0.0, 50.0} };
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glColor3f(0.0, 0.0, 1.0);
+	glViewport(0, 0, 300, 300);
+	triangle(verts);
+
+	glColor3f(1.0, 0.0, 0.0);
+	glViewport(300, 0, 300, 300);
+	glRotatef(90.0, 0.0, 0.0, 1.0);
+	triangle(verts);
+	glFlush();
+}
+void main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowPosition(50, 50);
+	glutInitWindowSize(600, 300);
+	glutCreateWindow("Split-Screen Example");
+
+	init();
+	glutDisplayFunc(displayFunc);
+
+	glutMainLoop();
+}
+#endif
+
+#ifdef CHAPTER_8_4_16_test
+class wcPt2D
+{
+public:
+	GLfloat x, y;
+};
+void init(void)
+{
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
 	gluOrtho2D(0.0, 100.0, 0.0, 50.0);
 	//glMatrixMode(GL_MODELVIEW);
 }
-wcPt2D verts[3] = { {10.0, 10.0},{40.0, 10.0},{25.0, 40.0} };
+wcPt2D verts[3] = { { 10.0, 10.0 },{ 40.0, 10.0 },{ 25.0, 40.0 } };
 wcPt2D verts2[3] = { { 60.0, 10.0 },{ 90.0, 10.0 },{ 75.0, 40.0 } };
 wcPt2D verts3[4] = { { 50.0, 0.0 },{ 100.0, 0.0 },{ 100.0, 50.0 },{ 50.0, 50.0 } };
-int w1, w2;
+int w1, w2, w3, w4;
 void displayFunc2(void);
 bool systemCall1 = true;
 bool systemCall2 = true;
-float curAngle = 10;
+float curAngle = 0;
 void triangle(wcPt2D * verts)
 {
 	float xc = 0, yc = 0;
@@ -40,7 +92,7 @@ void triangle(wcPt2D * verts)
 	}
 	xc = xc / 3;
 	yc = yc / 3;
-	
+
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	glTranslatef(xc, yc, 0);
@@ -52,12 +104,15 @@ void triangle(wcPt2D * verts)
 		glVertex2f(verts[k].x, verts[k].y);
 	glEnd();
 }
-void rect(wcPt2D * verts)
+void rectBorder(float left, float right, float bottom, float top)
 {
-	GLint k;
-	glBegin(GL_POLYGON);
-	for (k = 0; k < 4; k++)
-		glVertex2f(verts[k].x, verts[k].y);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glBegin(GL_LINE_LOOP);
+	glVertex2f(left + 1, bottom + 1);
+	glVertex2f(right - 1, bottom + 1);
+	glVertex2f(right - 1, top - 1);
+	glVertex2f(left + 1, top - 1);
 	glEnd();
 }
 void displayFunc1(void)
@@ -70,6 +125,8 @@ void displayFunc1(void)
 	glLoadIdentity();
 	gluOrtho2D(0.0, 100.0, 0.0, 50.0);
 	glViewport(0, 150, 600, 150);
+	glColor3f(0.0, 0.0, 0.0);
+	rectBorder(0.0, 100.0, 0.0, 50.0);
 	glColor3f(0.0, 0.0, 1.0);
 	triangle(verts);
 	//glColor3f(1.0, 1.0, 1.0);
@@ -81,18 +138,22 @@ void displayFunc1(void)
 	glLoadIdentity();
 	gluOrtho2D(0, 50.0, 0.0, 50.0);
 	glViewport(0, 0, 300, 150);
+	glColor3f(0.0, 0.0, 0.0);
+	rectBorder(0, 50.0, 0.0, 50.0);
 	glColor3f(0.0, 0.0, 1.0);
 	triangle(verts);
 	//glColor3f(1.0, 1.0, 1.0);
 	//rect(verts3);
 	glColor3f(1.0, 0.0, 0.0);
 	triangle(verts2);
-		
+
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluOrtho2D(50.0, 100.0, 0.0, 50.0);
 	glViewport(300, 0, 300, 150);
 	//glViewport(100, 0, 300, 150);
+	glColor3f(0.0, 0.0, 0.0);
+	rectBorder(50.0, 100.0, 0.0, 50.0);
 	glColor3f(0.0, 0.0, 1.0);
 	triangle(verts);
 	//glColor3f(1.0, 1.0, 1.0);
@@ -105,6 +166,7 @@ void displayFunc1(void)
 		systemCall2 = false;
 		glutSetWindow(w2);
 		glutPostRedisplay();
+
 	}
 
 	if (systemCall2)
@@ -130,33 +192,40 @@ void displayFunc2(void)
 
 	glClear(GL_COLOR_BUFFER_BIT);
 
+	// 以下显示同w1窗口，
 	//glMatrixMode(GL_PROJECTION);
 	//glLoadIdentity();
 	//gluOrtho2D(0.0, 100.0, 0.0, 50.0);
 	//glViewport(0, 150, 600, 150);
+	//glColor3f(0.0, 0.0, 0.0);
+	//rectBorder(0.0, 100.0, 0.0, 50.0);
 	//glColor3f(0.0, 0.0, 1.0);
 	//triangle(verts);
 	////glColor3f(1.0, 1.0, 1.0);
 	////rect(verts3);
 	//glColor3f(1.0, 0.0, 0.0);
 	//triangle(verts2);
-
+	//
 	//glMatrixMode(GL_PROJECTION);
 	//glLoadIdentity();
 	//gluOrtho2D(0, 50.0, 0.0, 50.0);
 	//glViewport(0, 0, 300, 150);
+	//glColor3f(0.0, 0.0, 0.0);
+	//rectBorder(0, 50.0, 0.0, 50.0);
 	//glColor3f(0.0, 0.0, 1.0);
 	//triangle(verts);
 	////glColor3f(1.0, 1.0, 1.0);
 	////rect(verts3);
 	//glColor3f(1.0, 0.0, 0.0);
 	//triangle(verts2);
-
+	//
 	//glMatrixMode(GL_PROJECTION);
 	//glLoadIdentity();
 	//gluOrtho2D(50.0, 100.0, 0.0, 50.0);
 	//glViewport(300, 0, 300, 150);
 	////glViewport(100, 0, 300, 150);
+	//glColor3f(0.0, 0.0, 0.0);
+	//rectBorder(50.0, 100.0, 0.0, 50.0);
 	//glColor3f(0.0, 0.0, 1.0);
 	//triangle(verts);
 	////glColor3f(1.0, 1.0, 1.0);
@@ -167,19 +236,26 @@ void displayFunc2(void)
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluOrtho2D(0.0, 100.0, 0.0, 50.0);
-	glViewport(0, 0, 600, 300);
+	//glViewport(0, 0, 600, 300);
+	glColor3f(0.0, 0.0, 0.0);
+	rectBorder(0.0, 100.0, 0.0, 50.0);
 	glColor3f(0.0, 0.0, 1.0);
 	triangle(verts);
 	//glColor3f(1.0, 1.0, 1.0);
 	//rect(verts3);
 	glColor3f(1.0, 0.0, 0.0);
 	triangle(verts2);
-	
+
 	if (systemCall2)
 	{
 		systemCall1 = false;
 		glutSetWindow(w1);
 		glutPostRedisplay();
+
+		//glutSetWindow(w3);
+		//glutPostRedisplay();
+		//glutReshapeWindow(400, 300);
+		//glutFullScreen();
 	}
 
 	if (systemCall1)
@@ -199,6 +275,65 @@ void displayFunc2(void)
 
 	glFlush();
 }
+void displayFunc3(void)
+{
+	int a = glutGetWindow();
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0, 100.0, 0.0, 50.0);
+	//glViewport(0, 0, 600, 300);
+	glColor3f(0.0, 0.0, 0.0);
+	rectBorder(0.0, 100.0, 0.0, 50.0);
+	glColor3f(0.0, 0.0, 1.0);
+	triangle(verts);
+	//glColor3f(1.0, 1.0, 1.0);
+	//rect(verts3);
+	glColor3f(1.0, 0.0, 0.0);
+	triangle(verts2);
+
+	//glutSetWindow(w4);
+	//glutReshapeWindow(400, 300);
+
+	glFlush();
+}
+void displayFunc4(void)
+{
+	int a = glutGetWindow();
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0, 100.0, 0.0, 50.0);
+	glViewport(0, 0, 300, 150);
+	glColor3f(0.0, 0.0, 0.0);
+	rectBorder(0.0, 100.0, 0.0, 50.0);
+	glColor3f(0.0, 0.0, 1.0);
+	triangle(verts);
+	//glColor3f(1.0, 1.0, 1.0);
+	//rect(verts3);
+	glColor3f(1.0, 0.0, 0.0);
+	triangle(verts2);
+
+	glFlush();
+}
+void winReshapeFcn2(GLint newWidth, GLint newHeight)
+{
+	//glViewport(0, 0, newWidth, newHeight);
+}
+void winReshapeFcn3(GLint newWidth, GLint newHeight)
+{
+	printf("winReshapeFcn4\n");
+	//glViewport(0, 0, newWidth, newHeight);
+}
+void winReshapeFcn4(GLint newWidth, GLint newHeight)
+{
+	printf("winReshapeFcn4\n");
+	//glViewport(0, 0, newWidth, newHeight);
+}
 void main(int argc, char** argv)
 {
 	int a = glutGetWindow();
@@ -216,11 +351,27 @@ void main(int argc, char** argv)
 	w2 = glutCreateWindow("Example2");
 	init();
 	glutDisplayFunc(displayFunc2);
+	glutReshapeFunc(winReshapeFcn2);
+	a = glutGetWindow();
+
+	glutInitWindowPosition(50, 800);
+	glutInitWindowSize(600, 300);
+	w3 = glutCreateWindow("Example3");
+	init();
+	glutDisplayFunc(displayFunc3);
+	glutReshapeFunc(winReshapeFcn3);
+	a = glutGetWindow();
+
+	w4 = glutCreateSubWindow(w3, 0, 0, 300, 150);
+	init();
+	glutDisplayFunc(displayFunc4);
+	glutReshapeFunc(winReshapeFcn4);
 	a = glutGetWindow();
 
 	glutMainLoop();
 }
 #endif
+
 
 //////////////////////////////////////////////////////////////////////////
 // CHAPTER_8_COMMON
@@ -245,10 +396,6 @@ void main(int argc, char** argv)
 	glutCreateWindow("An Example OpenGL Program");
 
 	init();
-
-//#ifdef CHAPTER_7_1_1
-//	code_7_1_1();
-//#endif
 
 	glutMainLoop();
 }
--- HelloOpenGL/opengl8h.h
+++ HelloOpenGL/opengl8h.h
@@ -1,9 +1,5 @@
 // 第八章代码
 
 // 选择一个，注释掉其他的
-#define CHAPTER_8_4_16 // 二维观察
-
-
-//#ifdef CHAPTER_7_1_1
-//#define CHAPTER_7_COMMON
-//#endif
+//#define CHAPTER_8_4_16 // 二维观察
+#define CHAPTER_8_4_16_test // 多视口，多窗口，子窗口等示例 二维观察
